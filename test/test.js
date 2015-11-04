640e6b7cbda6d8d8dcb7fd254463891a050b2f02
Josh Winskill <josh@tagcreativestudio.com>
2015-11-03 17:11:03 -0800

    added comments to test.js

--- test/test.js
+++ test/test.js
@@ -1,24 +1,36 @@
+/**
+ * 
+ * Test file for translate.js
+ * 
+ */
+
+/** dependencies */
 var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
 var Validator = require('jsonschema').Validator;
 var html5Lint = require('html5-lint');
 
+/** empty strings to be populated by HTML content */
 var badHTML = '';
 var inHTML  = '';
 var outHTML = '';
 var expHTML = '';
 
+/** empty strings to be populated by JSON content */
 var goodJSON = '';
 var badJSON  = '';
 
+/** empty arrays to be populated by HTML validation errors */
 var inErrors  = [];
 var outErrors = [];
 var badErrors = [];
 
+/** empty vars to be set to JSON validation error arrays */
 var goodJSONErrors;
 var badJSONErrors;
 
+/** Validator and schema for JSON validation */
 var v = new Validator();
 var schema = {
   id: '/locale',
@@ -32,7 +44,7 @@ var schema = {
       },
     },
   },
-
+  
 };
 
 /**
@@ -42,8 +54,15 @@ var schema = {
  * error arrays for testing in the test suite.
  *
  */
+
+/**
+ * void prepareTests
+ * @param function callback - the callback to be called when all async functions
+ * have been called
+ */
 var prepareTests = function(callback) {
 
+  /** HTML retrieval and validation */
   var filePath = path.join(__dirname, '../tmp/test/fixtures/index.html');
   fs.readFile(filePath, 'utf8', function(err, data) {
     if (!err) {
@@ -104,6 +123,7 @@ var prepareTests = function(callback) {
           var error = '' + type + ': ' + message;
           badErrors.push(error);
         });
+        /** callback called here to end async calls */
         callback();
       });
     } else {
@@ -112,6 +132,7 @@ var prepareTests = function(callback) {
 
   });
 
+  
   filePath = path.join(__dirname,
     '../tmp/test/fixtures/locales/de_DE/i18n.json');
   fs.readFile(filePath, 'utf8', function(err, data) {
@@ -135,9 +156,16 @@ var prepareTests = function(callback) {
 
 };
 
+/**
+ * The actual test suite to run
+ * 
+ * These test cases examine the error arrays that were generated
+ * earlier, and compares their length with the anticipated amount
+ * of returned errors
+ */
 describe('Mocha Test Suite', function() {
 
-  // Timeout increased to account for testing in the server
+  /** Timeout increased to account for testing in the server */
   this.timeout(25000);
 
   describe('hooks', function() {
@@ -171,8 +199,14 @@ describe('Mocha Test Suite', function() {
     });
 
     describe('check invalid JSON locale file for errors', function() {
-      it('errors should be returned', function() {
+      it('should contain errors', function() {
         assert.notEqual(0, badJSONErrors.errors.length);
+      });
+    });
+    
+    describe('Compare output HTML to expected HTML', function() {
+      it('should contain the same contents', function() {
+        assert.equal(outHTML, expHTML);        
       });
     });
 
